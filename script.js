2d9b5937b0bddc693363e0f8c15bd533ec97f562
CESAXX CODE <brziinspfc@gmail.com>
2025-12-29 14:36:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function contato() {
+    alert("Entre em contato pelo Discord! Basta clicar em ok que você será redirecionado para a página!");
+}
+
+function github() {
+    alert("Você está sendo redirecionado para www.github.com!")
+}
+
+function instagram() {
+    alert("Você está sendo redirecionado para o instagram!")
+}
