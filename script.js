817c08092665c71ce0478a45f4a0fc81d084ae91
CESAXX CODE <brziinspfc@gmail.com>
2025-12-29 14:35:33 -0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function contato() {
-    alert("Entre em contato pelo Discord! Basta clicar em ok que você será redirecionado para a página!");
-}
-
-function github() {
-    alert("Você está sendo redirecionado para www.github.com!")
-}
